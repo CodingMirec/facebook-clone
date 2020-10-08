7526b067f6a255b47b3f61ffa9da30fabf416bfb
CodingMirec <m.sykora01@gmail.com>
2020-10-08 19:35:55 +0200

    sidebar

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,34 @@
+import React from "react";
+// import { useStateValue } from "../StateProvider";
+
+import SidebarRow from "./SidebarRow";
+import "./Sidebar.css";
+
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
+import PeopleIcon from "@material-ui/icons/People";
+import ChatIcon from "@material-ui/icons/Chat";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import ExpandMoreOutlined from "@material-ui/icons/ExpandMore";
+import userEvent from "@testing-library/user-event";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <SidebarRow src="" title="Frankie" />
+      <SidebarRow
+        Icon={LocalHospitalIcon}
+        title="COVID-19 Information Center"
+      />
+      <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+      <SidebarRow Icon={PeopleIcon} title="Friends" />
+      <SidebarRow Icon={ChatIcon} title="Messenger" />
+      <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+      <SidebarRow Icon={VideoLibraryIcon} title="COVID-19 Information Center" />
+      <SidebarRow Icon={ExpandMoreOutlined} title="More" />
+    </div>
+  );
+};
+
+export default Sidebar;
