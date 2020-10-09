a44f1db0911e13f717a8421faa24f04caa5f833d
CodingMirec <m.sykora01@gmail.com>
2020-10-09 22:16:40 +0200

    MSender bot

--- src/Components/MessageSender.js
+++ src/Components/MessageSender.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import { Avatar, Input } from "@material-ui/core";
 
+import VideocamIcon from "@material-ui/icons/Videocam";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+
+import "./MessageSender.css";
+
 const MessageSender = () => {
   const [input, setInput] = useState("");
   const [image, setImage] = useState(null);
@@ -36,6 +42,21 @@ const MessageSender = () => {
             Hidden Submit
           </button>
         </form>
+      </div>
+
+      <div className="messageSender__bottom">
+        <div className="messageSender__option">
+          <VideocamIcon style={{ color: "red" }} />
+          <h3>Live Video</h3>
+        </div>
+        <div className="messageSender__option">
+          <PhotoLibraryIcon style={{ color: "green" }} />
+          <h3>Photo/Video</h3>
+        </div>
+        <div className="messageSender__option">
+          <InsertEmoticonIcon style={{ color: "orange" }} />
+          <h3>Feeling/Activity</h3>
+        </div>
       </div>
     </div>
   );
