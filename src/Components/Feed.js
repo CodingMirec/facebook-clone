e3fd24ef3ea83f0017fd24b456dcd55de6474519
CodingMirec <m.sykora01@gmail.com>
2020-10-11 11:34:17 +0200

    finished widgets - post

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MessageSender from "./MessageSender";
+import Post from "./Post";
 import StoryReel from "./StoryReel";
 
 const Feed = () => {
@@ -7,6 +8,13 @@ const Feed = () => {
     <div className="feed">
       <StoryReel />
       <MessageSender />
+      <Post
+        profilePic="https://icons-for-free.com/iconfiles/png/512/avatar-1320568024619304547.png"
+        message="yoo this is message"
+        timestamp="time"
+        imgName="imgName"
+        username="Frankie"
+      />
 
       {/* {postsData.map((entry) => (
         <Post
